ed5dcccf765177653fe3fbe59670cc697735e447
Katrina Scialdone <cj@swordglowsblue.com>
2020-07-08 10:07:15 -0600

    Channel and message typedefs

--- src/types/channels.ts
+++ src/types/channels.ts
@@ -1,4 +1,25 @@
-
-export type Channel = null
+import { PermissionOverwrite } from "./permissions.ts";
+import { User } from "./users.ts";
 
 export enum ChannelType { GUILD_TEXT, DM, GUILD_VOICE, GROUP_DM, GUILD_CATEGORY, GUILD_NEWS, GUILD_STORE }
+
+export type Channel = {
+  id: string
+  type: ChannelType
+  guild_id?: string
+  position?: number
+  permission_overwrites?: PermissionOverwrite[]
+  name?: string
+  topic?: string|null
+  nsfw?: boolean
+  last_message_id?: string|null
+  bitrate?: number
+  user_limit?: number
+  rate_limit_per_user?: number
+  recipients?: User[]
+  icon?: string|null
+  owner_id?: string
+  application_id?: string
+  parent_id?: string|null
+  last_pin_timestamp?: string
+}
--- src/types/messages.ts
+++ src/types/messages.ts
@@ -1,4 +1,120 @@
+import { User } from "./users.ts"
+import { GuildMember } from "./guilds.ts"
+import { ChannelType } from "./channels.ts"
 
-export type Message = null
-export type Embed = null
-export type Emoji = null
+export enum MessageType {
+  DEFAULT,
+  RECIPIENT_ADD,
+  RECIPIENT_REMOVE,
+  CALL,
+  CHANNEL_NAME_CHANGE,
+  CHANNEL_ICON_CHANGE,
+  CHANNEL_PINNED_MESSAGE,
+  GUILD_MEMBER_JOIN,
+  USER_PREMIUM_GUILD_SUBSCRIPTION,
+  USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_1,
+  USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_2,
+  USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_3,
+  CHANNEL_FOLLOW_ADD,
+  GUILD_DISCOVERY_DISQUALIFIED = 14,
+  GUILD_DISCOVERY_REQUALIFIED
+}
+
+export enum MessageFlags {
+  CROSSPOSTED            = 1 << 0,
+  IS_CROSSPOST           = 1 << 1,
+  SUPPRESS_EMBEDS        = 1 << 2,
+  SOURCE_MESSAGE_DELETED = 1 << 3,
+  URGENT                 = 1 << 4,
+}
+
+export type Message = {
+  id: string
+  channel_id: string
+  guild_id?: string
+  author: User | { id: string, username: string, avatar: string }
+  member?: GuildMember
+  content: string
+  timestamp: string
+  edited_timestamp: string|null
+  tts: boolean
+  mention_everyone: boolean
+  mentions: (User & { member?: GuildMember })[]
+  mention_roles: string[]
+  mention_channels?: { id: string, guild_id: string, name: string, type: ChannelType }[]
+  attachments: any[] // TODO: implement
+  embeds: Embed[]
+  reactions?: any[] // TODO: implement
+  nonce?: string|number
+  pinned: boolean
+  webhook_id?: string
+  type: MessageType
+  activity?: any[] // TODO: implement
+  application?: any[] // TODO: implement
+  message_reference?: any[] // TODO: implement
+  flags?: MessageFlags
+}
+
+export type Embed = {
+  title?: string
+  type?: string
+  description?: string
+  url?: string
+  timestamp?: string
+  color?: number
+
+  footer?: {
+    text: string
+    icon_url?: string
+    proxy_icon_url?: string
+  }
+
+  image?: {
+    url?: string
+    proxy_url?: string
+    height?: number
+    width?: number
+  }
+
+  thumbnail?: {
+    url?: string
+    proxy_url?: string
+    height?: number
+    width?: number
+  }
+
+  video?: {
+    url?: string
+    height?: number
+    width?: number
+  }
+
+  provider?: {
+    name?: string
+    url?: string
+  }
+
+  author?: {
+    name?: string
+    url?: string
+    icon_url?: string
+    proxy_icon_url?: string
+  }
+
+  fields?: {
+    name: string
+    value: string
+    inline?: boolean
+  }[]
+}
+
+export type Emoji = {
+  id: string|null
+  name: string|null
+  roles?: string[]
+  user?: User
+  require_colons?: boolean
+  managed?: boolean
+  animated?: boolean
+  available?: boolean
+}
